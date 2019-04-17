800c87089bd51c0aad7a8473207d5b9e8e1726fe
David <davidaase@hotmail.com>
2019-04-17 12:48:43 +0200

    add code examples

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,56 @@
 package app;
 
+import io.javalin.Context;
 import io.javalin.Javalin;
+import io.javalin.UnauthorizedResponse;
+import io.javalin.security.Role;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+
         Javalin app = Javalin.create()
             .sessionHandler(Sessions::fileSessionHandler)
             .start(7000);
+
+        app.get("/write", ctx -> {
+            // values written to the session will be available on all your instances if you use a session db
+            ctx.sessionAttribute("my-key", "My value");
+        });
+
+        app.get("/read", ctx -> {
+            // values on the session will be available on all your instances if you use a session db
+            String myValue = ctx.sessionAttribute("my-key");
+        });
+
+        app.get("/invalidate", ctx -> {
+            // if you want to invalidate a session, jetty will clean everything up for you
+            ctx.req.getSession().invalidate();
+        });
+
+        app.get("/change-id", ctx -> {
+            // it could be wise to change the session id on login, to protect against session fixation attacks
+            ctx.req.changeSessionId();
+        });
+
+        app.accessManager((handler, ctx, roles) -> {
+            String currentUser = ctx.sessionAttribute("current-user"); // retrieve user stored during login
+            if (currentUser == null) {
+                redirectToLogin(ctx);
+            } else if (userHasValidRole(ctx, roles)) {
+                handler.handle(ctx);
+            } else {
+                throw new UnauthorizedResponse();
+            }
+        });
+
     }
+
+    private static boolean userHasValidRole(Context ctx, Set<Role> roles) {
+        return false; // your code here
+    }
+
+    private static void redirectToLogin(Context ctx) {
+    }
+
 }
--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,9 +1,46 @@
 package app
 
+import io.javalin.Context
 import io.javalin.Javalin
+import io.javalin.UnauthorizedResponse
+import io.javalin.security.Role
 
 fun main(args: Array<String>) {
+
     val app = Javalin.create().apply {
         sessionHandler(::fileSessionHandler)
     }.start(7000)
+
+    app.get("/write") { ctx ->
+        // values written to the session will be available on all your instances if you use a session db
+        ctx.sessionAttribute("my-key", "My value")
+    }
+
+    app.get("/read") { ctx ->
+        // values on the session will be available on all your instances if you use a session db
+        val myValue = ctx.sessionAttribute<String>("my-key")
+    }
+
+    app.get("/invalidate") { ctx ->
+        // if you want to invalidate a session, jetty will clean everything up for you
+        ctx.req.session.invalidate()
+    }
+
+    app.get("/change-id") { ctx ->
+        // it could be wise to change the session id on login, to protect against session fixation attacks
+        ctx.req.changeSessionId()
+    }
+
+    app.accessManager { handler, ctx, roles ->
+        val currentUser = ctx.sessionAttribute<String?>("current-user") // retrieve user stored during login
+        when {
+            currentUser == null -> redirectToLogin(ctx)
+            currentUser != null && userHasValidRole(ctx, roles) -> handler.handle(ctx)
+            else -> throw UnauthorizedResponse()
+        }
+    }
+
 }
+
+fun redirectToLogin(ctx: Context) = ctx.redirect("/login")
+fun userHasValidRole(ctx: Context, roles: Set<Role>) = false // your code here
